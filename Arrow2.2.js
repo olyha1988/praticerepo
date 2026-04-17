c625998d5555e5324f387784e4957417ef05c68b
olyha1988 <olyhavy@gmail.com>
2026-04-17 07:50:06 -0400

    Implement console

---
+++ Arrow2.2.js
@@ -0,0 +1,8 @@
+//convert MunitestoSecond
+const convertToSeconds=minutes=>minutes*60;
+//GST calcution
+const addGST=price=>price+price*0.18;
+//Login Validation
+const validationLogin=(username,password)=>username==="admin" && password==="1234";
+//Cart Item Court
+const getcartCount=Items=>Items.length;
---
+++ var2.1.js
@@ -0,0 +1,10 @@
+let users=[
+    {id:1, name:"Olivier", active:true},
+    {id:2, name:"Livvy",active:false}];
+let total=0;
+for(let i=0;i<users.length;i++){
+
+ total++;
+}
+
+console.log('Total: ${total}');
